670d64491c4f422fbe8292a814e3bb759ceaceb2
Kevin Oliver <koliver@mercurypay.com>
2013-05-02 17:07:57 -0600

    First Commit

---
+++ TranSentryIPADHID/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TranSentryIPADHID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSwiped = new System.Windows.Forms.Button();
+            this.btnKeyed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSwiped
+            // 
+            this.btnSwiped.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSwiped.Location = new System.Drawing.Point(13, 13);
+            this.btnSwiped.Name = "btnSwiped";
+            this.btnSwiped.Size = new System.Drawing.Size(140, 101);
+            this.btnSwiped.TabIndex = 0;
+            this.btnSwiped.Text = "Swiped";
+            this.btnSwiped.UseVisualStyleBackColor = true;
+            this.btnSwiped.Click += new System.EventHandler(this.btnSwiped_Click);
+            // 
+            // btnKeyed
+            // 
+            this.btnKeyed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeyed.Location = new System.Drawing.Point(12, 120);
+            this.btnKeyed.Name = "btnKeyed";
+            this.btnKeyed.Size = new System.Drawing.Size(140, 101);
+            this.btnKeyed.TabIndex = 1;
+            this.btnKeyed.Text = "Keyed";
+            this.btnKeyed.UseVisualStyleBackColor = true;
+            this.btnKeyed.Click += new System.EventHandler(this.btnKeyed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(165, 234);
+            this.Controls.Add(this.btnKeyed);
+            this.Controls.Add(this.btnSwiped);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSwiped;
+        private System.Windows.Forms.Button btnKeyed;
+    }
+}
+
---
+++ TranSentryIPADHID/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TranSentryIPADHID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSwiped_Click(object sender, EventArgs e)
+        {
+            ProcessIPAD(false);
+        }
+
+        private void btnKeyed_Click(object sender, EventArgs e)
+        {
+            ProcessIPAD(true);
+        }
+
+        private static void ProcessIPAD(bool isManual)
+        {
+            //Declare initial objects
+            Transaction.Request saleRequest = new Transaction.Request();
+            Transaction.PinPadConfig pinPadConfig = new Transaction.PinPadConfig();
+            MercuryShield.TranSentry tranSentry = new MercuryShield.TranSentry();
+            Transaction.Response response = new Transaction.Response();
+            String responseText = String.Empty;
+
+            MPS.POS.Utils.Logging.LogData("Here is some information", "DataLog");
+            
+            //Populate base Request fields common to all transactions (i.e. config file values)
+            saleRequest.CreditServers = "x1.mercurydev.net;x2.mercurydev.net";
+            saleRequest.MerchantID = "395347308=E2ETKN";
+            saleRequest.Memo = "[Product, Version]"; // Supply your POS product and version number
+            saleRequest.UserID = "Test"; // UserID PA-DSS requirement logging
+
+            //Populate unique per request fields.  POS Supplied.
+            saleRequest.InvoiceNumber = "1";
+            saleRequest.ReferenceNumber = "1";
+            saleRequest.AmountData.PurchaseAmount = 1.25;
+            saleRequest.TokenFrequency = MSEnum.TokenFrequencies.OneTime;
+
+            //The following per-request fields define the request type.
+            saleRequest.ImplementationType = "E2E";
+            saleRequest.TenderType = MSEnum.TenderTypes.Credit;
+            saleRequest.TransactionType = MSEnum.TransactionTypes.Sale;
+
+            if (isManual)
+            {
+                saleRequest.ProcessingSetName = "MagTekIPAD Manual";
+            }
+            else
+            {
+                saleRequest.ProcessingSetName = "MagTekIPAD Swiped";
+            }
+
+            // Set the PinPadConfig Fields used by E2E and pass to the Request
+            pinPadConfig.TimeOutValue = 30;
+            pinPadConfig.DelayBeforeSendingDataToDevice = 0;
+            pinPadConfig.CaptureSignature = true;
+            pinPadConfig.PinPadType = Transaction.PinPadType.Unknown;
+            saleRequest.PinPadConfig = pinPadConfig;
+         
+            // Set the request object in the TranSentry object, process, and capture the response.
+            tranSentry.TranSentryRequest = saleRequest;
+            tranSentry.ProcessRequest();
+            response = tranSentry.TranSentryResponse;
+
+            //
+            //Handle the response however you wish.
+            //
+            responseText = "Credit Sale Response" + Environment.NewLine;
+            responseText = responseText + Environment.NewLine;
+            responseText = responseText + "CmdStatus: " + response.CmdStatus + Environment.NewLine;
+            responseText = responseText + "TextResponse: " + response.TextResponse + Environment.NewLine;
+            responseText = responseText + "ProcessData: " + response.ProcessData;
+
+            MessageBox.Show(responseText, "TranSentry");
+        }
+
+        
+    }
+}
